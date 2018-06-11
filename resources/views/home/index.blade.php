b8738d9fd27144f07e2ebca82fb94b1e9fddb93d
Richard <richardtekel@me.com>
2018-06-11 10:39:19 +0200

    init

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Practice</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+<div id="app">
+    <h1>Home page</h1>
+
+    <example-component></example-component>
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+</html>
